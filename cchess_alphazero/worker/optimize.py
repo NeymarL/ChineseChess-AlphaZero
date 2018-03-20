19706adc9794964d05af349656e82e603fd59802
NeymarL <lh57506155@qq.com>
2018-03-20 09:40:53 +0800

    fix bugs

--- cchess_alphazero/worker/optimize.py
+++ cchess_alphazero/worker/optimize.py
@@ -50,7 +50,7 @@ class OptimizeWorker:
         while True:
             files = get_game_data_filenames(self.config.resource)
             if (len(files) * self.config.play_data.nb_game_in_file < self.config.trainer.min_games_to_begin_learn \
-              or ((last_file is not None) and files.index(last_file) + 20 > len(files))):
+              or ((last_file is not None) and files.index(last_file) + 100 > len(files))):
                 if last_file is not None:
                     logger.info('Waiting for enough data 600s, ' + str((len(files) - files.index(last_file)) * self.config.play_data.nb_game_in_file) \
                             +' vs '+ str(self.config.trainer.min_games_to_begin_learn)+' games')
