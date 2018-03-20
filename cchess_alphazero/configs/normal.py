99fe50d664cb8af3d04d9cbc7d4e38d6f6584b42
NeymarL <lh57506155@qq.com>
2018-03-20 12:35:25 +0800

    fix bugs

--- cchess_alphazero/configs/normal.py
+++ cchess_alphazero/configs/normal.py
@@ -33,7 +33,7 @@ class PlayDataConfig:
 class PlayConfig:
     def __init__(self):
         self.max_processes = 10
-        self.search_threads = 8
+        self.search_threads = 6
         self.vram_frac = 1.0
         self.simulation_num_per_move = 50
         self.thinking_loop = 1
