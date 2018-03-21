273d127028c6b557118b5b2693b293c0c93af3a4
NeymarL <lh57506155@qq.com>
2018-03-21 19:10:19 +0800

    pause and think a moment

--- cchess_alphazero/agent/player.py
+++ cchess_alphazero/agent/player.py
@@ -1,14 +1,14 @@
 from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor
 from logging import getLogger
-from threading import Lock
+from threading import Lock, Condition
 
 import numpy as np
 
 from cchess_alphazero.config import Config
 from cchess_alphazero.environment.env import CChessEnv
 from cchess_alphazero.environment.lookup_tables import Winner, ActionLabelsRed, flip_policy, flip_move, flip_action_labels
-from time import time
+from time import time, sleep
 
 logger = getLogger(__name__)
 
@@ -19,6 +19,7 @@ class VisitState:
         self.visit = []                     # thread id that has visited this state
         self.p = None                       # policy of this state
         self.legal_moves = None             # all leagal moves of this state
+        self.waiting = False                # is waiting for NN's predict
 
 
 class ActionState:
@@ -36,7 +37,7 @@ class CChessPlayer:
         self.labels_n = len(ActionLabelsRed)
         self.labels = ActionLabelsRed
         self.move_lookup = {move: i for move, i in zip(self.labels, range(self.labels_n))}
-        self.pipe_pool = pipes              # pipes that used to communicate with CChessModelAPI thread
+        self.pipe = pipes                   # pipes that used to communicate with CChessModelAPI thread
         self.node_lock = defaultdict(Lock)  # key: state key, value: Lock of that state
 
         if search_tree is None:
@@ -50,6 +51,23 @@ class CChessPlayer:
         self.search_results = {}        # for debug
         self.debug = {}
 
+        self.s_lock = Lock()
+        self.run_lock = Lock()
+        self.q_lock = Lock()            # queue lock
+        self.t_lock = Lock()
+        self.buffer_planes = []         # prediction queue
+        self.buffer_history = []
+
+        self.all_done = Lock()
+        self.num_task = 0
+
+        self.job_done = False
+
+        self.executor = ThreadPoolExecutor(max_workers=self.play_config.search_threads+2)
+        self.executor.submit(self.receiver)
+        self.executor.submit(self.sender)
+        self.pp = None
+
     def get_state_key(self, env: CChessEnv) -> str:
         board = env.observation
         board = board.split(' ')
@@ -60,6 +78,45 @@ class CChessPlayer:
         if not env.red_to_move:
             legal_moves = flip_action_labels(legal_moves)
         return legal_moves
+
+    def close(self):
+        self.job_done = True
+        if self.executor is not None:
+            self.executor.shutdown()
+
+    def sender(self):
+        '''
+        send planes to neural network for prediction
+        '''
+        limit = 256                 # max prediction queue size
+        while not self.job_done:
+            self.run_lock.acquire()
+            with self.q_lock:
+                l = min(limit, len(self.buffer_history))
+                if l > 0:
+                    t_data = self.buffer_planes[0:l]
+                    self.pipe.send(t_data)
+                else:
+                    self.run_lock.release()
+                    sleep(0.001)
+
+    def receiver(self):
+        '''
+        receive policy and value from neural network
+        '''
+        while not self.job_done:
+            if self.pipe.poll(0.001):
+                rets = self.pipe.recv()
+            else:
+                continue
+            k = 0
+            with self.q_lock:
+                for ret in rets:
+                    self.executor.submit(self.update_tree, ret[0], ret[1], self.buffer_history[k])
+                    k = k + 1
+                self.buffer_planes = self.buffer_planes[k:]
+                self.buffer_history = self.buffer_history[k:]
+            self.run_lock.release()
 
     def action(self, env: CChessEnv) -> str:
         value = self.search_moves(env)  # MCTS search
@@ -78,61 +135,54 @@ class CChessPlayer:
         self.moves.append([env.observation, list(policy)])    # do not need flip anymore when training
         return self.labels[my_action]
 
-    def search_moves(self, env: CChessEnv) -> float:
-        '''
-        apply parallel MCTS, return max leaf value of all simulations
-        '''
-        futures = []
-        with ThreadPoolExecutor(max_workers=self.play_config.search_threads) as executor:
-            for i in range(self.play_config.simulation_num_per_move):
-                futures.append(executor.submit(self.MCTS_search, env=env.copy(), is_root_node=True, tid=i))
-
-        vals = [f.result() for f in futures]
-        return np.max(vals)
-
-    def MCTS_search(self, env: CChessEnv, is_root_node=False, tid=0) -> float:
+    def MCTS_search(self, env: CChessEnv, is_root_node=False, history=[]) -> float:
         """
         Monte Carlo Tree Search
         """
-        if env.done:
-            if env.winner == Winner.draw:
-                return 0
-            elif (env.winner == Winner.red and env.red_to_move) or (env.winner == Winner.black and not env.red_to_move):
-                return 1
-            else:
-                return -1
+        while True:
+            if env.done:
+                if env.winner == Winner.draw:
+                    v = 0
+                elif (env.winner == Winner.red and env.red_to_move) or (env.winner == Winner.black and not env.red_to_move):
+                    v = 1
+                else:
+                    v = -1
+                self.executor.submit(self.update_tree, None, v, history)
+                break
 
-        state = self.get_state_key(env)
+            state = self.get_state_key(env)
 
-        with self.node_lock[state]:
-            if state not in self.tree:
-                # Expand and Evaluate
-                leaf_p, leaf_v = self.expand_and_evaluate(env)
-                if self.debugging:
-                    # for debug -------------------------
-                    self.debug[state] = (leaf_p, leaf_v)
-                    # -----------------------------------
-                self.tree[state].p = leaf_p
-                self.tree[state].legal_moves = self.get_legal_moves(env)
-                return leaf_v
+            with self.node_lock[state]:
+                if state not in self.tree:
+                    # Expand and Evaluate
+                    self.tree[state].sum_n = 1
+                    self.tree[state].legal_moves = self.get_legal_moves(env)
+                    self.expand_and_evaluate(state, history)
+                    break
 
-            if tid in self.tree[state].visit:   # loop
-                return 0
+                if state in history[:-1]: # loop -> loss
+                    self.executor.submit(self.update_tree, None, 0, history)
+                    break
 
-            # Select
-            self.tree[state].visit.append(tid)
-            sel_action = self.select_action_q_and_u(state, is_root_node)
-            
-            if sel_action is None:
-                return -1
+                # Select
+                node = self.tree[state]
+                if node.waiting:
+                    node.visit.append(history)
+                    break
 
-            virtual_loss = self.config.play.virtual_loss
-            self.tree[state].sum_n += virtual_loss
-            
-            action_state = self.tree[state].a[sel_action]
-            action_state.n += virtual_loss
-            action_state.w -= virtual_loss
-            action_state.q = action_state.w / action_state.n
+                sel_action = self.select_action_q_and_u(state, is_root_node)
+                is_root_node = False
+                
+                if sel_action is None:
+                    return -1
+
+                virtual_loss = self.config.play.virtual_loss
+                self.tree[state].sum_n += virtual_loss
+                
+                action_state = self.tree[state].a[sel_action]
+                action_state.n += virtual_loss
+                action_state.w -= virtual_loss
+                action_state.q = action_state.w / action_state.n
 
         if env.red_to_move:
             env.step(sel_action)
