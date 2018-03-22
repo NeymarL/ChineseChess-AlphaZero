288a75029c7cd8491820fbc5bdad6d6d6b09e474
NeymarL <lh57506155@qq.com>
2018-03-22 11:01:32 +0800

    update configs

--- cchess_alphazero/configs/normal.py
+++ cchess_alphazero/configs/normal.py
@@ -2,7 +2,7 @@ class EvaluateConfig:
     def __init__(self):
         self.vram_frac = 1.0
         self.game_num = 10
-        self.simulation_num_per_move = 50 # before 200
+        self.simulation_num_per_move = 800 # before 200
         self.thinking_loop = 1
         self.c_puct = 1 # lower  = prefer mean action value
         self.tau_decay_rate = 0
@@ -35,7 +35,7 @@ class PlayConfig:
         self.max_processes = 10
         self.search_threads = 6
         self.vram_frac = 1.0
-        self.simulation_num_per_move = 50
+        self.simulation_num_per_move = 800
         self.thinking_loop = 1
         self.logging_thinking = False
         self.c_puct = 1.5
