210581548593af2173e554662376867834b57acc
NeymarL <lh57506155@qq.com>
2018-03-27 13:16:23 +0800

    update observe self play with cli

--- cchess_alphazero/play_games/ob_self_play.py
+++ cchess_alphazero/play_games/ob_self_play.py
@@ -54,8 +54,8 @@ class ObSelfPlay:
             if action is None:
                 print("AI投降了!")
                 break
+            move = self.env.board.make_single_record(int(move[0]), int(move[1]), int(move[2]), int(move[3]))
             self.env.step(action)
-            move = self.env.board.make_single_record(int(action[0]), int(action[1]), int(action[2]), int(action[3]))
             print(f"AI选择移动 {move}")
             self.env.board.print_to_cl()
             sleep(1)
