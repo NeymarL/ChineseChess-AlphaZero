a1debbbd20f8d652d9b37bb2fe6c661249d0be81
NeymarL <lh57506155@qq.com>
2018-04-11 08:57:02 +0800

    update file reader

--- cchess_alphazero/lib/data_helper.py
+++ cchess_alphazero/lib/data_helper.py
@@ -10,7 +10,8 @@ logger = getLogger(__name__)
 
 def get_game_data_filenames(rc: ResourceConfig):
     pattern = os.path.join(rc.play_data_dir, rc.play_data_filename_tmpl % "*")
-    files = list(sorted(glob(pattern)))
+    files = list(sorted(glob(pattern)), key=get_key)
+    files.reverse()
     return files
 
 def write_game_data_to_file(path, data):
@@ -22,3 +23,6 @@ def read_game_data_from_file(path):
     with open(path, "rt") as f:
         return json.load(f)
 
+def get_key(x):
+    stat_x = os.stat(x) 
+    return stat_x.st_ctime
