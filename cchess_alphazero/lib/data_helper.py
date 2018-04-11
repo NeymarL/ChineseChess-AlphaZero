a7b198640072771ba88b237992f5257b3c1e0acd
NeymarL <lh57506155@qq.com>
2018-04-11 10:16:40 +0800

    debug

--- cchess_alphazero/lib/data_helper.py
+++ cchess_alphazero/lib/data_helper.py
@@ -11,7 +11,6 @@ logger = getLogger(__name__)
 def get_game_data_filenames(rc: ResourceConfig):
     pattern = os.path.join(rc.play_data_dir, rc.play_data_filename_tmpl % "*")
     files = list(sorted(glob(pattern), key=get_key))
-    files.reverse()
     return files
 
 def write_game_data_to_file(path, data):
