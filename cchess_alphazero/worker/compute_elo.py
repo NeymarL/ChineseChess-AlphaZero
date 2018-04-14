12cfd1a0525feed01b5c7e2683ef4dc97d45edcf
NeymarL <lh57506155@qq.com>
2018-04-14 16:26:30 +0800

    debug

--- cchess_alphazero/worker/compute_elo.py
+++ cchess_alphazero/worker/compute_elo.py
@@ -107,7 +107,7 @@ class EvaluateWorker:
         if idx == 0:
             _, new_elo = compute_elo(int(self.data['base']['elo']), int(self.data['unchecked']['elo']), score)
         else:
-            new_elo, _ = compute_elo(int(self.data['unchecked']['elo']), int(self.data['base']['elo']), 1 - score)
+            new_elo, _ = compute_elo(int(self.data['unchecked']['elo']), int(self.data['base']['elo']), score)
 
         relative_elo = new_elo - int(self.data['unchecked']['elo'])
         logger.info(f"进程{self.pid}评测完毕 用时{(end_time - start_time):.1f}秒, "
