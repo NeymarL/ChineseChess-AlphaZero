566e0c9d5a9e0b504ac941763e05f52aafd00ccf
NeymarL <lh57506155@qq.com>
2018-04-16 12:59:20 +0800

    change to 128x7

--- cchess_alphazero/configs/distribute.py
+++ cchess_alphazero/configs/distribute.py
@@ -6,7 +6,7 @@ class EvaluateConfig:
         self.thinking_loop = 1
         self.c_puct = 1 # lower  = prefer mean action value
         self.tau_decay_rate = 0
-        self.noise_eps = 0.15
+        self.noise_eps = 0.1
         self.max_game_length = 100
         self.max_processes = 6
         self.search_threads = 20
