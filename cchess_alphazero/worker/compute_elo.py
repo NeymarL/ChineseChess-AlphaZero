1abf367773ae89830613987667e9228e58c51a0b
NeymarL <lh57506155@qq.com>
2018-04-16 15:34:42 +0800

    debug

--- cchess_alphazero/worker/compute_elo.py
+++ cchess_alphazero/worker/compute_elo.py
@@ -215,9 +215,6 @@ def load_model(config, weight_path, digest, config_file=None):
         except ValueError as e:
             logger.error(f"权重架构不匹配，自动重新加载 {e}")
             return load_model(config, weight_path, digest, 'model_256f.json')
-        except tensorflow.python.framework.errors_impl.InvalidArgumentError as e:
-            logger.error(f"权重架构不匹配，自动重新加载 {e}")
-            return load_model(config, weight_path, digest, 'model_256f.json')
         except Exception as e:
             logger.error(f"加载权重发生错误：{e}，10s后自动重试下载")
             os.remove(weight_path)
