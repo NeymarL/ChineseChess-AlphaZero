d29caaaf5abc92953a9dc51949a59e278a8d571e
NeymarL <lh57506155@qq.com>
2018-04-20 11:35:31 +0800

    fix load weight bug

--- cchess_alphazero/lib/model_helper.py
+++ cchess_alphazero/lib/model_helper.py
@@ -52,7 +52,7 @@ def load_model_weight(model, config_path, weight_path, name=None):
     try:
         return  model.load(config_path, weight_path)
     except ValueError:
-        config_path = os.path.join(config.resource.model_dir, 'model_256f.json')
+        config_path = os.path.join(model.config.resource.model_dir, 'model_256f.json')
         return load_model_weight(model, config_path, weight_path)
 
 def save_as_next_generation_model(model):
