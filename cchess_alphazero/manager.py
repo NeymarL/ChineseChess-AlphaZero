7a0d82453cd7e3a07c64946cace3f084d24a8ab4
NeymarL <lh57506155@qq.com>
2018-05-10 12:18:11 +0800

    fix bugs

--- cchess_alphazero/manager.py
+++ cchess_alphazero/manager.py
@@ -98,7 +98,7 @@ def start():
             if mp.get_start_method() == 'spawn':
                 import cchess_alphazero.worker.compute_elo_windows as evaluator
             else:
-                from cchess_alphazero.worker.compute_elo import evaluator
+                import cchess_alphazero.worker.compute_elo as evaluator
         config.eval.update_play_config(config.play)
         evaluator.start(config)
     elif args.cmd == 'sl':
